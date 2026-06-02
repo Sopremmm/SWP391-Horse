5d9dcb45c51669f64dccf1aa8cec36a09fad32b1
Akira174 <tuanpg2016@gmail.com>
2026-06-02 10:17:21 +0700

    Fix route /register and update Login/Register components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./Login";
-import RegisterPage from "./Register";
+import Login from "./Login";
+import Register from "./Register";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/Register" element={<RegisterPage />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
