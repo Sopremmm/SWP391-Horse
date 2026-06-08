97c460b96c5ec2f44d7b0efd727295b82a7cb102
Akira174 <tuanpg2016@gmail.com>
2026-06-08 08:01:02 +0700

    Front-end: commit all changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from './App.tsx';
 
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error('Root element not found');
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(<App />);
